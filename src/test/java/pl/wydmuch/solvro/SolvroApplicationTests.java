c6a0d08bbecefadd9a5d2ccd84a7bfa41b231d19
Patryk Wydmuch <pvydmuch@gmail.com>
2019-10-18 17:31:09 +0200

    Initial

---
+++ src/test/java/pl/wydmuch/solvro/SolvroApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wydmuch.solvro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
