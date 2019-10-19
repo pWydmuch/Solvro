fa72b29ce6d1cd7cf03c673a65f17d464d37584d
Patryk Wydmuch <pvydmuch@gmail.com>
2019-10-19 21:44:40 +0200

    Add email & password validation

--- src/main/java/pl/wydmuch/solvro/dto/UserDto.java
+++ src/main/java/pl/wydmuch/solvro/dto/UserDto.java
@@ -1,25 +1,20 @@
 package pl.wydmuch.solvro.dto;
 
+import pl.wydmuch.solvro.validators.ValidEmail;
+import pl.wydmuch.solvro.validators.ValidPassword;
+
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 public class UserDto {
-    @NotBlank
-    private String userName;
-    @NotBlank
-    private String password;
-//    private String matchingPassword;
-    @NotBlank
+
+    @ValidEmail
     private String email;
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+    @ValidPassword
+    private String password;
 
     public String getPassword() {
         return password;
@@ -35,5 +30,13 @@ public class UserDto {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
