7bf7f6a9a96ecea5fe7c34aea0fd210a57c07ac6
Patryk Wydmuch <pvydmuch@gmail.com>
2019-10-28 00:17:53 +0100

    Add docker compose

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
-wait-for-it.sh db:3306 -t 30
-java -Djava.security.egd=file:/dev/./urandom  -jar solvro-0.0.1-SNAPSHOT.jar
+#! /bin/bash
+./wait-for-it.sh mypostgres:5432 -t 15
+java -Djava.security.egd=file:/dev/./urandom -jar app.jar
