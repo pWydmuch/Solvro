bf64493243a93790968d4b86fb7e621d20ecd3a5
Patryk Wydmuch <pvydmuch@gmail.com>
2019-10-28 14:58:14 +0100

    Chmod ./mvnw

--- run-app.sh
+++ run-app.sh
@@ -5,6 +5,7 @@ status="$(docker-machine status)"
 if [ "$status" == "Stopped" ] ; then
     docker-machine start
 fi
+chmod u+x ./mvnw
 ./mvnw clean install -DskipTests
 cd src/main/docker
 docker-compose rm -v
